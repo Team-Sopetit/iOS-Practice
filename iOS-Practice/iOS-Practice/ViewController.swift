5246ceebb8d4806d3b13a0aa4e05fa3856348c12
ahra1221 <lizze011221@gmail.com>
2023-12-30 15:50:04 +0900

    [Setting] 초기세팅

---
+++ iOS-Practice/iOS-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Practice
+//
+//  Created by 고아라 on 2023/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
